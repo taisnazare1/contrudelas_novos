746a7f8ce2bed59add8bfd932d05da5452c362c6
taisnazare1 <taisnazare_1@hotmail.com>
2021-10-06 21:42:18 -0300

    commit html

---
+++ js/projeto.js
@@ -0,0 +1,100 @@
+nome1 = prompt("Qual é o nome do aluno1?");
+notas = prompt("digite as notas do aluno?");
+notas = notas.split(",");
+
+nota1 = notas[0]
+nota2 = notas [1]
+nota3 = notas [2]
+
+nota1 =  parseFloat (nota1)
+nota2 = parseFloat (nota2) 
+nota3 = parseFloat (nota3)
+
+media1 = (nota1 + nota2 + nota3 )/ 3
+
+nome2 = prompt("Qual é o nome do aluno2?");
+notas = prompt("digite as notas do aluno?");
+notas = notas.split(",");
+
+nota1 = notas[0]
+nota2 = notas [1]
+nota3 = notas [2]
+
+nota1 =  parseFloat (nota1)
+nota2 = parseFloat (nota2) 
+nota3 = parseFloat (nota3)
+
+media2 = (nota1 + nota2 + nota3 )/ 3
+
+nome3 = prompt("Qual é o nome do aluno3?");
+notas = prompt("digite as notas do aluno?");
+notas = notas.split(",");
+
+nota1 = notas[0]
+nota2 = notas [1]
+nota3 = notas [2]
+
+nota1 =  parseFloat (nota1)
+nota2 = parseFloat (nota2) 
+nota3 = parseFloat (nota3)
+
+media3 = (nota1 + nota2 + nota3 )/ 3
+
+nome4 = prompt("Qual é o nome do aluno4?");
+notas = prompt("digite as notas do aluno?");
+notas = notas.split(",");
+
+nota1 = notas[0]
+nota2 = notas [1]
+nota3 = notas [2]
+
+nota1 =  parseFloat (nota1)
+nota2 = parseFloat (nota2) 
+nota3 = parseFloat (nota3)
+
+media4 = (nota1 + nota2 + nota3 )/ 3
+
+nome5 = prompt("Qual é o nome do aluno5?");
+notas = prompt("digite as notas do aluno?");
+notas = notas.split(",");
+
+nota1 = notas[0]
+nota2 = notas [1]
+nota3 = notas [2]
+
+nota1 =  parseFloat (nota1)
+nota2 = parseFloat (nota2) 
+nota3 = parseFloat (nota3)
+
+media5= (nota1 + nota2 + nota3 )/ 3
+
+
+ console.log (media1)
+ if (media1 >= 5)
+console.log (nome1 +" Aprovado")
+else
+console.log (nome1 +" Reprovado")
+    
+ console.log (media2)
+ if (media2 >= 5)
+console.log (nome2 +" Aprovado")
+else
+console.log (nome2 +" Reprovado")
+
+ console.log (media3)
+ if (media3 >= 5)
+console.log (nome3 +" Aprovado")
+else
+console.log (nome3 +" Reprovado")
+
+console.log (media4)
+ if (media4 >= 5)
+console.log (nome4 +" Aprovado")
+else
+console.log (nome4 +" Reprovado")
+
+console.log (media5)
+ if (media5 >= 5)
+console.log (nome5 +" Aprovado")
+else
+console.log (nome5 +" Reprovado")
